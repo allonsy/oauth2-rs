8afc6db8355b068e858d25c83fe5fd56bebcab28
Alec Snyder <linuxbash8@gmail.com>
2016-06-22 13:30:21 -0700

    update docs

--- implementors/oauth2/trait.Authorization.js
+++ implementors/oauth2/trait.Authorization.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['oauth2'] = [];
+implementors['oauth2'] = [];implementors['oauth2'] = [];implementors['oauth2'] = [];implementors['oauth2'] = [];implementors['oauth2'] = [];implementors['oauth2'] = [];implementors['oauth2'] = [];implementors['oauth2'] = [];implementors['oauth2'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- oauth2/sidebar-items.js
+++ oauth2/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Config","Configuration of an oauth2 application."],["Token",""]],"trait":[["Authorization","Helper trait for extending the builder-style pattern of curl::Request."]]});
+initSidebarItems({"struct":[["Config","Configuration of an oauth2 application."],["Token","Represents a Token struct"]],"trait":[["Authorization","Helper trait for extending the builder-style pattern of curl::easy::Easy."]]});
