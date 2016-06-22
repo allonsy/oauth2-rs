9dafb3f3a95252c09e58e9fbf68cd55e4e489569
Alec Snyder <linuxbash8@gmail.com>
2016-06-22 13:28:31 -0700

    add more docs and examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,45 @@
 #![cfg_attr(test, deny(warnings))]
+//! A library for making oauth2 requests with updated depencies like curl 0.3.0
+//!
+//! An example:
+//!
+//! ```rust,no_run
+//! extern crate rustc_serialize;
+//! extern crate oauth2;
+//!
+//! use rustc_serialize::json;
+//! use std::fs::File;
+//! use std::io::Read;
+//! /* Secrets.json sample contents:
+//! {
+//!   "client_id": "abcde",
+//!   "client_secret": "efgab",
+//!   "auth_url": "https://github.com/login/oauth/authorize",
+//!   "token_url": "https://github.com/login/oauth/access_token"
+//! }
+//! */
+//! let mut f = File::open("secrets.json").unwrap();
+//! let mut read_str = String::new();
+//! let _ = f.read_to_string(&mut read_str);
+//! let sec : Secret = json::decode(&read_str).unwrap();
+//!
+//! let mut conf = oauth2::Config::new(
+//!     &sec.client_id,
+//!     &sec.client_secret,
+//!     &sec.auth_url,
+//!     &sec.token_url
+//! );
+//! conf.scopes = vec!["repo".to_owned()];
+//! let url = conf.authorize_url("v0.0.1 gitbot".to_owned());
+//! println!("please visit this url: {}", url);
+//!
+//! let mut user_code = String::new();
+//! let _ = std::io::stdin().read_line(&mut user_code).unwrap();
+//! user_code.pop();
+//! let tok = conf.exchange(user_code).unwrap();
+//! println!("access code is: {}", tok.access_token);
+//! ```
+//!
 
 extern crate url;
 extern crate curl;
